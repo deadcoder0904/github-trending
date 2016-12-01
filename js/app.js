16941b1f4b6b4c3996d32dd5989fda569fdd08cf
Akshay Kadam <akshaykadam0904@gmail.com>
2016-12-02 00:05:13 +0530

    Adding script on Fri Dec  2 00:05:13 IST 2016 :uk:

--- js/app.js
+++ js/app.js
@@ -3,22 +3,22 @@ function handleData(day, data) {
 	var json = JSON.parse(data);
 	var c = 0;
 	var trending = $('#trending');
-	
+
 	arr.push("<h1 class='blue bg-black-40 dib pa4'> <i class='em em-alien'></i> " + day.slice(0,-5) + " <i class='em em-skull'></i></h1>");
   for(var i = 0; i < json.length; i++) {
 		if(c % 2 == 0) {
 			arr.push("<div class='dt dt--fixed'>");
   		arr.push("<div class='dt-row'>");
   	}
-		 
+
 		arr.push("<div class='dtc tc pv4 bg-black-40'>");
 		arr.push("<a class='f3 link b hover-orange no-underline dark-green dib ph2 pv1 ba b--purple-20' href='" + json[i].url + "' target='_blank'>" + json[i].name + "</a>");
 		arr.push("<p class='f3 lh-copy tc'>" + json[i].description + "</p>");
 	  arr.push("</div>");
-  
+
 		if((c + 1) % 2 == 0) {
   		arr.push("</div>");
-			arr.push("</div>");	
+			arr.push("</div>");
   	}
   	c++;
   }
@@ -26,14 +26,14 @@ function handleData(day, data) {
 }
 
 $(document).ready(() => {
-	
-	$('body').flowtype({ 
-	 minimum : 300, 
-	 maximum : 1200 
+
+	$('body').flowtype({
+	 minimum : 300,
+	 maximum : 1200
 	});
 
 	let trendsDate = [];
-	
+
 	$.ajax({
 	  	url: "https://api.github.com/repos/deadcoder0904/github-trending-json-api/git/trees/master?recursive=1"
 		}).done(function(res) {
@@ -44,16 +44,21 @@ $(document).ready(() => {
 			   		for(i = 0; i < res1.tree.length; i++) {
 			   			trendsDate.push(res1.tree[i].path);
 						}
+						trendsDate.sort(function(a, b){
+					    const aa = a.split('-').reverse().join(),
+					        bb = b.split('-').reverse().join();
+					    return aa < bb ? -1 : (aa > bb ? 1 : 0);
+						});
 			   		for (i = trendsDate.length - 1; i >= 0; i--) {
 		   				let date = trendsDate[i];
 			   			$.getJSON({
-								  	url: 'https://raw.githubusercontent.com/deadcoder0904/github-trending-json-api/master/json/' + date 
+								  	url: 'https://raw.githubusercontent.com/deadcoder0904/github-trending-json-api/master/json/' + date
 									}).done(function(res2) {
 						   			handleData(date,JSON.stringify(res2));
 							}).catch(function(err2) {
 									console.log(err2);
 							});
-		   			}	
+		   			}
 				}).catch(function(err1) {
 						console.log(err1);
 				});
